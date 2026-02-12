e36b7379af6dade91c8966ef41accfb0c6bc8169
ozay.goktas <ogoktas@gokhanboya.com>
2026-02-12 13:37:00 +0300

    nokta listesi webde listelendi

---
+++ GuvenlikKontrolWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GuvenlikKontrol.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace GuvenlikKontrolWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nokta> Noktalar { get; set; }
+    }
+
+}
--- GuvenlikKontrolWeb/Pages/Index.cshtml.cs
+++ GuvenlikKontrolWeb/Pages/Index.cshtml.cs
@@ -1,20 +1,42 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
 
 namespace GuvenlikKontrolWeb.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public List<Nokta> NoktaListesi { get; set; } = new List<Nokta>();
 
         public void OnGet()
         {
+            string connectionString = "Server=OZAY\\DATA;Database=GuvenlikKontrol;User Id=data;Password=data1234;TrustServerCertificate=True;";
 
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT NoktaID, NoktaAdi FROM NoktaTanimlari";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        NoktaListesi.Add(new Nokta
+                        {
+                            NoktaID = dr.GetInt32(0),
+                            NoktaAdi = dr.GetString(1)
+                        });
+                    }
+                }
+            }
         }
+    }
+
+    public class Nokta
+    {
+        public int NoktaID { get; set; }
+        public string NoktaAdi { get; set; }
     }
 }

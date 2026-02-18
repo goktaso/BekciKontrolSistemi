7f62bb7d67a71b60996c6955dc44e86e06e684e8
ozay.goktas <ogoktas@gokhanboya.com>
2026-02-18 17:55:15 +0300

    Add project files.

---
+++ Pages/BekciPerformans.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GuvenlikKontrolWeb.Pages
+{
+    public class BekciPerformansModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

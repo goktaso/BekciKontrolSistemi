90190a446a8318111d4f2410b02526541649e3ec
ozay.goktas <ogoktas@gokhanboya.com>
2026-02-23 14:09:51 +0300

    Atılmayan Tur sayfasıda OK_23.02_14.09

---
+++ Pages/AtilmayanTur.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GuvenlikKontrolWeb.Pages
+{
+    public class AtilmayanTurModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

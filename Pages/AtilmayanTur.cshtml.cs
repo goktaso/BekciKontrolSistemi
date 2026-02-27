4dbb3021c94c09c2c2610859861fa2d97979f5d9
ozay.goktas <ogoktas@gokhanboya.com>
2026-02-27 10:09:32 +0300

    Atılmayan Tur yeniden ok

--- Pages/AtilmayanTur.cshtml.cs
+++ Pages/AtilmayanTur.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace GuvenlikKontrolWeb.Pages
+namespace YourProject.Pages
 {
     public class AtilmayanTurModel : PageModel
     {
+        // Veri listesi buraya gelecek (Örn: List<Tur> Turlar)
+
         public void OnGet()
         {
+            // Sayfa yüklendiðinde çalýþacak kodlar
+        }
+
+        // Excel dýþa aktarma iþlemi için handler
+        public IActionResult OnPostExportExcel()
+        {
+            // Excel oluþturma mantýðý
+            return File(new byte[0], "application/vnd.ms-excel", "AtilmayanTurlar.xlsx");
         }
     }
 }
